6a5e9cc367d3841971eb8418ac6cc36899b4056c
Saharsh Diwan <saharsh804@gmail.com>
2023-09-13 22:15:43 +0530

    do

--- app.js
+++ app.js
@@ -45,15 +45,54 @@
 //     console.log(`Server is running on port ${port}`);
 // });
 
-    var firebaseConfig = {
-        apiKey: "YOUR_API_KEY",
-        authDomain: "YOUR_AUTH_DOMAIN",
-        projectId: "YOUR_PROJECT_ID",
-        storageBucket: "YOUR_STORAGE_BUCKET",
-        messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-        appId: "YOUR_APP_ID"
-    };
+    // var firebaseConfig = {
+    //     apiKey: "AIzaSyBu9TrmL8ED1p7FWye70QPCpQ_PkC29qlY",
+    //     authDomain: "YOUR_AUTH_DOMAIN",
+    //     projectId: "funchick-c1ba8",
+    //     storageBucket: "gs://funchick-c1ba8.appspot.com",
+    //     messagingSenderId: "326986025678",
+    //     appId: "YOUR_APP_ID"
+    // };
 
-    // Initialize Firebase
-    firebase.initializeApp(firebaseConfig);
+    // // Initialize Firebase
+    // firebase.initializeApp(firebaseConfig);
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBu9TrmL8ED1p7FWye70QPCpQ_PkC29qlY",
+  authDomain: "funchick-c1ba8.firebaseapp.com",
+  projectId: "funchick-c1ba8",
+  storageBucket: "funchick-c1ba8.appspot.com",
+  messagingSenderId: "326986025678",
+  appId: "1:326986025678:web:ea32000e1cdcac3a631e9f",
+  measurementId: "G-BMX079ZXF2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+/*  
+
+    // Initialize a variable to store the user's selected rating
+    
+        var fullName = document.getElementById('full_name').value;
+        var email = document.getElementById('email').value;
+        var message = document.getElementById('message').value;
+
+        if (selectedRating !== null) {
+            // Rating is selected; you can now use 'selectedRating' to store it in Firebase
+            console.log('Selected Rating:', selectedRating);
+            
+            // Your Firebase code to store the review data here
+        } else {
+            console.error('Please select a rating.');
+        }
+    });*/
